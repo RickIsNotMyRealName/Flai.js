323eecc5724c0292bf98b1b51f881fdd84810d29
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2025-06-09 13:59:41 -0600

    feat: make palette expandable button

--- src/components/NodePalette.tsx
+++ src/components/NodePalette.tsx
@@ -1,12 +1,10 @@
 import { useMemo, useState } from 'react';
 import { useWorkflowStore } from '../store/workflowStore';
-import type { NodeType } from '../types';
-import { clsx } from 'clsx';
 
 export default function NodePalette() {
   const nodeTypes = useWorkflowStore((s) => s.nodeTypes);
-  const addNode = useWorkflowStore((s) => s.addNode);
   const [query, setQuery] = useState('');
+  const [open, setOpen] = useState(false);
 
   const filtered = useMemo(() => {
     const q = query.toLowerCase();
@@ -18,26 +16,41 @@ export default function NodePalette() {
   }, [nodeTypes, query]);
 
   return (
-    <aside className="palette">
-      <input
-        placeholder="Search..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <ul>
-        {filtered.map((nt) => (
-          <li
-            key={nt.id}
-            draggable
-            onDragStart={(e) => {
-              e.dataTransfer.setData('application/x-node-type', nt.id);
-            }}
+    <div className="palette-wrapper">
+      {!open && (
+        <button className="palette-toggle" onClick={() => setOpen(true)}>
+          Nodes
+        </button>
+      )}
+      {open && (
+        <aside className="palette palette-floating">
+          <button
+            className="palette-close"
+            onClick={() => setOpen(false)}
           >
-            {nt.icon && <img src={nt.icon} alt="" />}
-            {nt.name}
-          </li>
-        ))}
-      </ul>
-    </aside>
+            ✕
+          </button>
+          <input
+            placeholder="Search..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <ul>
+            {filtered.map((nt) => (
+              <li
+                key={nt.id}
+                draggable
+                onDragStart={(e) => {
+                  e.dataTransfer.setData('application/x-node-type', nt.id);
+                }}
+              >
+                {nt.icon && <img src={nt.icon} alt="" />}
+                {nt.name}
+              </li>
+            ))}
+          </ul>
+        </aside>
+      )}
+    </div>
   );
 }
