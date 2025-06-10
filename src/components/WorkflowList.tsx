80d6090b7b0637fd724f1e77478cd50af5d004e1
RickIsNotMyRealName <54247719+RickIsNotMyRealName@users.noreply.github.com>
2025-06-09 23:21:57 -0600

    Clarify workflow creation button

--- src/components/WorkflowList.tsx
+++ src/components/WorkflowList.tsx
@@ -61,6 +61,7 @@ export default function WorkflowList({
               strokeLinecap="round"
             />
           </svg>
+          <span className="create-label">New</span>
         </button>
       </div>
       <ul>
